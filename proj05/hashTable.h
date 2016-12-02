9f746132a16edb89e2cf625ff55ec7cba54e2f86
Hong Wing PANG <hwpang@ust.hk>
2016-12-02 20:47:43 +0800

    Update

--- proj05/hashTable.h
+++ proj05/hashTable.h
@@ -9,19 +9,91 @@ class HashTable {
 		HashElem();
 		HashElem(const Key&, const Value&);
 		~HashElem();
+		Key getKey();
+		Value getValue();
 	};
 private:
 	int size; //no of linked lists
 	list<HashElem> * table; //Array of linked lists
 public:
 	HashTable();
-	HashTable(int size);
-	//copy ctor
+	HashTable(int s);
+	//HashTable(const HashTable&);
 	~HashTable();
 	
 	int getHashVal(int);
 	int getHashVal(string);
 	
-	T getElem(T elem);
-	void putElem(T elem);
+	int getSize();
+	//HashElem getElem(HashElem elem);
+	void putElem(HashElem elem);
 };
+
+//HashTable class members
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashTable() : size(0) {}
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashTable(int s) : size(s) {
+	table = new std::list<HashElem>[size];
+}
+
+/*
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashTable(const HashTable& ht) : size(ht.getSize()) {
+	//copy
+}
+*/
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::~HashTable() {
+	delete [] table;
+}
+
+template <typename Key, typename Value>
+int HashTable<Key, Value>::getHashVal(int key) {
+	return key % size;
+}
+
+template <typename Key, typename Value>
+int HashTable<Key, Value>::getHashVal(string key) {
+	//foobar
+}
+
+template <typename Key, typename Value>
+int HashTable<Key, Value>::getSize() {
+	return size;
+}
+
+template <typename Key, typename Value>
+void HashTable<Key, Value>::putElem(HashElem elem) {
+	std::list<HashElem>::iterator l;
+	
+	int val = getHashVal(elem.getKey());
+	table[val]->push_back(elem);
+}
+
+//HashElem class members
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashElem::HashElem() {}
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashElem::HashElem(const Key& k, const Value& v)
+: _key(k), _value(v) {}
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashElem::~HashElem() {}
+
+template <typename Key, typename Value>
+Key HashTable<Key, Value>::HashElem::getKey() {
+	return _key;
+}
+
+template <typename Key, typename Value>
+Value HashTable<Key, Value>::HashElem::getValue() {
+	return _value;
+}
+
+//perform validation here later
